3122ef23c5e34565d62311483e2f6525b94ad508
ArianaNChaves <navi.ariana99@gmail.com>
2024-05-27 17:23:34 -0300

    Fix nexo now can get damage

--- Assets/Nexo/Nexo.cs
+++ Assets/Nexo/Nexo.cs
@@ -5,9 +5,15 @@ namespace Nexo
 {
     public class Nexo : MonoBehaviour, IDamageable
     {
+        [SerializeField] private float health;
         public void TakeDamage(float amount)
         {
             Debug.Log($"NEXO {gameObject.name} recibio  {amount} damage");
+            health -= amount;
+            if (health <= 0)
+            {
+                Debug.Log("GAME OVER");
+            }
         }
     }
 }
