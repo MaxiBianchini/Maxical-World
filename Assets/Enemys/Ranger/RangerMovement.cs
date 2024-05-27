c304d0eb86523c4b7a6a2c0a56b8b3ce3f191509
ArianaNChaves <navi.ariana99@gmail.com>
2024-05-27 17:08:49 -0300

    Update Rangers attack nexo only mele

--- Assets/Enemys/Ranger/RangerMovement.cs
+++ Assets/Enemys/Ranger/RangerMovement.cs
@@ -11,6 +11,7 @@ namespace Enemys.Ranger
     public class RangerMovement : MonoBehaviour, IDamageable, IEnemy
     {
         [SerializeField] private float attackRange;
+        [SerializeField] private float meleAttack;
         [SerializeField] private float attackSpeed;
         [SerializeField] private float rotationSpeed;
         
@@ -105,6 +106,11 @@ namespace Enemys.Ranger
             {
                 _destination = _target.transform.position;
                 _agent.SetDestination(_destination);
+                
+                if (_target.CompareTag("Nexo"))
+                {
+                    attackRange = meleAttack;
+                }
             }
         }
         
@@ -167,14 +173,22 @@ namespace Enemys.Ranger
         
         private void CheckRangeAndAttack()
         {
+            
             float distanceToDestination = Vector3.Distance(transform.position, _destination);
-
-          //  LookTarget();
+            LookTarget();
+            
             if (distanceToDestination <= attackRange && !_isAttacking)
             {
-                _isAttacking = true;
-                _agent.isStopped = true;
-                StartAttacking();
+              //  if (IsTargetVisible())
+              //  {
+                    _isAttacking = true;
+                    _agent.isStopped = true;
+                    StartAttacking();
+               // }
+               // else
+               // {
+               //    Debug.Log($"No se ve el target");
+               // }
             }
             
             else if (distanceToDestination > attackRange)
@@ -184,15 +198,30 @@ namespace Enemys.Ranger
                 StopAttacking();
             }
         }
-     /*   private void LookTarget()
+        private void LookTarget()
         {
-            if (_isAttacking) //mira al obeetivo
+            if (_isAttacking && _target != null) //mira al obeetivo
             {
                 _direction = _target.transform.position - transform.position;
                 _targetRotation = Quaternion.LookRotation(_direction);
                 transform.rotation = Quaternion.Slerp(transform.rotation, _targetRotation, Time.deltaTime * rotationSpeed);
             }
         }
+    /*    
+        private bool IsTargetVisible()
+        {
+            Vector3 directionToTarget = _target.transform.position - transform.position;
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, directionToTarget, out hit, attackRange))
+            {
+                if (hit.transform == _target.transform)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
         */
         
         private IEnumerator AttackPerform()
@@ -201,7 +230,7 @@ namespace Enemys.Ranger
             {
                 yield return new WaitForSeconds(10/attackSpeed);
                 Attack(_target);
-                Debug.Log($"Attack {_target}");
+                //Debug.Log($"Attack {_target}");
             }
         }
     }
